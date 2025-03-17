3b2c01c732fccdd8b1178ecfdeb1d57808acddc6
PrAtHaM-0707 <prathamm4402@gmail.com>
2025-03-17 18:20:00 +0530

    Update commandicons.js

--- UI/icons/commandicons.js
+++ UI/icons/commandicons.js
@@ -1,6 +1,7 @@
 const cmdIcons = {
     dotIcon: 'https://cdn.discordapp.com/emojis/915658913850998794.gif',
     serverinfoIcon : 'https://cdn.discordapp.com/emojis/942629018296025128.gif',
+    YouTubeIcon : 'https://cdn.discordapp.com/emojis/1229455855192379514.png',
     FaceBookIcon : 'https://cdn.discordapp.com/emojis/788969884062711818.png',
     TwitchIcon : 'https://cdn.discordapp.com/emojis/1229455826356404275.gif',
     InstagramIcon : 'https://cdn.discordapp.com/emojis/1188371472104837191.png',
