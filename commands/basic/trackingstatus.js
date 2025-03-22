b8f7629f6ba71abe7dbf5bb763da8074466427e0
PrAtHaM-0707 <prathamm4402@gmail.com>
2025-03-22 18:36:35 +0530

    Update trackingstatus.js

--- commands/basic/trackingstatus.js
+++ commands/basic/trackingstatus.js
@@ -1,30 +1,38 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { userPreferencesCollection } = require('../../mongodb'); // Adjust the path to point to mongodb.js
+const { userPreferencesCollection } = require('../../mongodb');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('trackingstatus')
-        .setDescription('Check if you have opted out of message tracking.'),
+        .setDescription('Check if a user has opted out of message tracking.')
+        .addUserOption(option =>
+            option
+                .setName('user')
+                .setDescription('The user to check the tracking status for. Defaults to you.')
+                .setRequired(false)
+        ),
     async execute(interaction) {
         // Check if the user is the bot owner (ID: 1095038359480574102)
         if (interaction.user.id !== '1095038359480574102') {
             return await interaction.reply({
                 content: '❌ You do not have permission to use this command. This is an owner-only command.',
-                ephemeral: true // Makes the reply visible only to the user
+                ephemeral: true
             });
         }
 
-        const userId = interaction.user.id;
+        // Get the user to check (default to the command issuer if no user is specified)
+        const targetUser = interaction.options.getUser('user') || interaction.user;
+        const userId = targetUser.id;
 
         // Check the user's opt-out status
         const userPreference = await userPreferencesCollection.findOne({ userId: userId });
         const isOptedOut = userPreference?.optedOut || false;
 
-        // Reply to the user
+        // Reply to the owner
         if (isOptedOut) {
-            await interaction.reply('You are currently **opted out** of message tracking.');
+            await interaction.reply(`${targetUser.tag} is currently **opted out** of message tracking.`);
         } else {
-            await interaction.reply('You are currently **opted in** to message tracking.');
+            await interaction.reply(`${targetUser.tag} is currently **opted in** to message tracking.`);
         }
     },
 };
