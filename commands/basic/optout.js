3a7544ba589c221c2f23f207736a2f3b211a29d7
PrAtHaM-0707 <prathamm4402@gmail.com>
2025-03-22 18:35:48 +0530

    Update optout.js

--- commands/basic/optout.js
+++ commands/basic/optout.js
@@ -1,29 +1,37 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { userPreferencesCollection } = require('../../mongodb'); // Adjust the path to point to mongodb.js
+const { userPreferencesCollection } = require('../../mongodb');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('optout')
-        .setDescription('Stop the bot from tracking your messages.'),
+        .setDescription('Stop a user from having their messages tracked.')
+        .addUserOption(option =>
+            option
+                .setName('user')
+                .setDescription('The user to opt out of message tracking.')
+                .setRequired(true)
+        ),
     async execute(interaction) {
         // Check if the user is the bot owner (ID: 1095038359480574102)
         if (interaction.user.id !== '1095038359480574102') {
             return await interaction.reply({
                 content: '❌ You do not have permission to use this command. This is an owner-only command.',
-                ephemeral: true // Makes the reply visible only to the user
+                ephemeral: true
             });
         }
 
-        const userId = interaction.user.id;
+        // Get the user to opt out
+        const targetUser = interaction.options.getUser('user');
+        const userId = targetUser.id;
 
         // Save the user's opt-out preference in the database
         await userPreferencesCollection.updateOne(
             { userId: userId },
             { $set: { optedOut: true } },
-            { upsert: true } // Create a new entry if the user doesn't exist
+            { upsert: true }
         );
 
-        // Reply to the user
-        await interaction.reply('You have opted out of message tracking. The bot will no longer track your messages.');
+        // Reply to the owner
+        await interaction.reply(`✅ ${targetUser.tag} has been opted out of message tracking.`);
     },
 };
